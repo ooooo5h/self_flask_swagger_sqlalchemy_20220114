c14eafec5dc9c8d74d160ccbfe8a3470829be804
ooooo5h <ooooo5h91@gmail.com>
2022-01-14 18:00:06 +0900

    기본은, 실제 환경으로 설정
    AWS EC2에서 배포할때를 고려한 세팅

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 from server import create_app
 
-app = create_app()
+app = create_app('ProductionConfig')
 
 
 # 디버그모드 => 파이선 파일을 저장하면, 서버도 자동 재시작
--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-def create_app(config_name)):
+def create_app(config_name):
     app = Flask(__name__)
     
     # 만들어진 앱에 환경설정을 불러오자
