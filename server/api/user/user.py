0a83fe4066ed3a67e79fdd00dbc8c7bf0760b50c
전은형 <ooooo5h91@gmail.com>
2022-01-14 21:40:25 +0900

    사용자 정보 조회 문서 작업 기초
    파라미터 요구 X
    응답 경우별 예시 등록 X
    어떤 기능인지 명시 X

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,7 +1,23 @@
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class User(Resource):
     
+    @swagger.doc({
+        'tags' : ['user'],
+        'description' : '사용자 정보 조회',
+        'parameters' : [
+            # dict로 파라미터들 명시
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '사용자 정보 조회 성공',
+            },
+            '400' : {
+                'description' : '사용자 정보 조회 실패',
+            }
+        }
+    })
     def get(self):
         return {
             '임시' : '사용자 정보 조회'
