ca6b784fcb6c3c5fd827d9c4ec76119471146430
ooooo5h <ooooo5h91@gmail.com>
2022-01-14 18:07:23 +0900

    python manager.py "키워드"에 따라 다른 환경 실행 세팅
    flask_script / Manager 클래스 기능 활용

--- manager.py
+++ manager.py
@@ -1,7 +1,21 @@
 from server import create_app
+from flask_script import Manager
 
 app = create_app('ProductionConfig')
 
+manager = Manager(app)
 
-# 디버그모드 => 파이선 파일을 저장하면, 서버도 자동 재시작
-app.run(host='0.0.0.0')
+@manager.command
+def debug():
+    # 추가키워드 debug
+    app.config.from_object('server.config.DebugConfig')
+    app.run(host='0.0.0.0')
+    
+@manager.command
+def runserver():
+    # 추가키워드 runserver
+    app.run(host='0.0.0.0')
+
+# 이 파일이 실행될때는 매니저의 커맨드를 기반으로 실행되게 해야한다
+if __name__ == '__main__':
+    manager.run()
