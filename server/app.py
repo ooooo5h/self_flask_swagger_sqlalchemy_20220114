b6c66cd72dde12bd6d8f2cc42f99529af552ce40
ooooo5h <ooooo5h91@gmail.com>
2022-01-14 18:15:47 +0900

    USER Api클래스를 가져다가 api변수에 등록

--- server/app.py
+++ server/app.py
@@ -12,4 +12,8 @@ def create_app(config_name):
     # 클래스에 있는 함수들을 자동으로 기능으로 연결해주는 라이브러리 세팅 및 부가 환경설정도 진행
     api = Api(app, api_spec_url = '/api/spec', title='my_server_spec', api_version = '0.1', catch_all_404s = True)
     
+    from server.api.user import User
+    
+    api.add_resource(User, '/user')
+    
     return app
