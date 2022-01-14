682f31b6c5c3b8292bcf7243c6788e9a2f6b12ab
전은형 <ooooo5h91@gmail.com>
2022-01-14 21:44:59 +0900

    각 기능 요약 문구를 추가
    swagger 기능 표시 문구 추가

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -1,7 +1,26 @@
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class Lecture(Resource):
+    
+    
+    @swagger.doc({
+        'tags' : ['lecture'],
+        'description' : '수강취소',
+        'parameters' : [
+            
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '수강 취소 성공',
+            },
+            '400' : {
+                'description' : '수강 취소 실패',
+            }
+        }
+    })    
     def delete(self):
+        """수강 취소"""
         return{
             '임시' : '수강취소!'
         }
--- server/api/user/user.py
+++ server/api/user/user.py
@@ -19,6 +19,7 @@ class User(Resource):
         }
     })
     def get(self):
+        """사용자 정보 조회"""
         return {
             '임시' : '사용자 정보 조회'
         }
@@ -40,6 +41,7 @@ class User(Resource):
         }
     })    
     def post(self):
+        """로그인"""
         return{
             '임시' : '로그인 기능'
         }
@@ -61,6 +63,7 @@ class User(Resource):
         }
     })        
     def put(self):
+        """회원 가입"""
         return{
             '임시' : '회원가입'
         }
