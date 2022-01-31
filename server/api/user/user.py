52a27fee707e046a940aeab815ad268c690503c6
전은형 <ooooo5h91@gmail.com>
2022-01-31 18:43:48 +0900

    사용자 정보 조회 문서 작업 기초
    - 파라미터 요구 x
    - 응답 경우별 예시 등록 x
    - 어떤 기능인지 명시 x

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,9 +1,26 @@
 # 사용자에 관련된 기능을 수행하는 클래스
 # 메쏘드를 만들 때, get/post/put/patch/delete로 만들면, 알아서 메쏘드로 세팅되도록
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class User(Resource):
     
+    @swagger.doc({
+      'tags' : ['user'], # 어떤 종류의 기능인지 분류
+      'description' : '사용자 정보 조회',
+      'parameters' : [
+          # dict로 파라미터들을 명시
+      ],
+      'responses' : {
+          # 200 일때의 응답 예시, 400일때의 예시 등
+          '200' : {
+              'description' : '사용자 정보 조회에 성공'
+          },
+          '400' : {
+              'description' : '사용자 정보 조회 실패' 
+          }
+      }
+    })
     def get(self):
         return {
             '임시' : '사용자 정보 조회'
