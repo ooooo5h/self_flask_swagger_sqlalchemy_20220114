5677f2914cbfed30ff1ba5a5139c34be4713fa74
전은형 <ooooo5h91@gmail.com>
2022-01-31 18:45:55 +0900

    강의 관련 기능은 swagger 태그 분리

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -1,7 +1,24 @@
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class Lecture(Resource):
     
+    @swagger.doc({
+      'tags' : ['lecture'], # 어떤 종류의 기능인지 분류
+      'description' : '수강 취소',
+      'parameters' : [
+          # dict로 파라미터들을 명시
+      ],
+      'responses' : {
+          # 200 일때의 응답 예시, 400일때의 예시 등
+          '200' : {
+              'description' : '수강 취소 성공'
+          },
+          '400' : {
+              'description' : '수강 취소 실패' 
+          }
+      }
+    })  
     def delete(self):
         return {
             '임시' : '수강 취소 기능'
