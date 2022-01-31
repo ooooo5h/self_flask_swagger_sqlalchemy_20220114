f84240363735129a324fe812d192821a8e39bb79
전은형 <ooooo5h91@gmail.com>
2022-01-31 18:28:31 +0900

    클래스를 API로 활용하도록 도와주는 세팅

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_restful_swagger_2 import Api
 
 def create_app(config_name):
     app = Flask(__name__)
     
     # 만들어진 앱에 (server > config > 환경클래스)환경설정을 불러오자
     app.config.from_object(f'server.config.{config_name}')
+    
+    # 클래스에 있는 함수들을 자동으로 기능으로 연결해주는 라이브러리 세팅 + 부가 환경설정 진행
+    api = Api(app, api_spec_url = '/api/spec', title='my_server spec', api_version='0.1', catch_all_404s=True)
     
     return app
