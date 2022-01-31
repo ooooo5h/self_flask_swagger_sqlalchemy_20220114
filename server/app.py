eddc63e84667f003577d81a37be563d07f2a040b
전은형 <ooooo5h91@gmail.com>
2022-01-31 18:32:30 +0900

    User API 클래스를 가져다가 api변수에 등록
    /user 주소로 연결

--- server/app.py
+++ server/app.py
@@ -10,4 +10,9 @@ def create_app(config_name):
     # 클래스에 있는 함수들을 자동으로 기능으로 연결해주는 라이브러리 세팅 + 부가 환경설정 진행
     api = Api(app, api_spec_url = '/api/spec', title='my_server spec', api_version='0.1', catch_all_404s=True)
     
+    from server.api.user import User
+    
+    # api폴더에서 만든 User클래스를 가져다가 /user로 접속 가능하게 등록
+    api.add_resource(User, '/user')
+    
     return app
